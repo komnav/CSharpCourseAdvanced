7d412c8cd84ce7dc5c40cf5a51ddc73dd8e2802b
dilovar komilov <komilov.dilovar2001@mail.ru>
2024-07-11 16:13:54 +0500

    DI

--- day_7/DI/DI/Program.cs
+++ day_7/DI/DI/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using DI;
+using Microsoft.Extensions.DependencyInjection;
+
+IServiceCollection services = new ServiceCollection();
+
+services.AddTransient<NotificationService>();
+services.AddTransient<IMessageService, EmailService>();
+services.AddTransient<ILogger, ConsoleLogger>();
+services.AddTransient<IDataBase, PostgreDataBase>();
+
+var serviceProvider = services.BuildServiceProvider();
+var notificationservice = serviceProvider.GetRequiredService<NotificationService>();
+
+notificationservice.Notify();
+
