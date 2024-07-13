9cd0af3ca779f828fc1d57117a5b0639c84528ad
Navruz <navruzkomilov503@gmail.com>
2024-07-13 18:02:05 +0500

    day8

---
+++ day_8/FirstRestAPI/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstRestAPI.Controllers
+{
+    [ApiController]
+    [Route("prod")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("Navruz")]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public ActionResult Get2()
+        {
+            return Ok();
+        }
+    }
+}
